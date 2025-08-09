aa336c4ab25304b1a6856f53bcad70be0dbbe553
Mert <mertarslan260@gmail.com>
2025-08-09 21:29:40 +0300

    Refactor main and add print function with switch
    Moved print1 and print2 function definitions, added a new print() function that prompts the user for input and calls print1 or print2 based on the input using a switch statement. Updated main() to call print() instead of print1().

--- UdemyCourse1/main.c
+++ UdemyCourse1/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    print1();
-    return 0;
-}
 
 void print1()
+{
+    printf("David says , \" Programming is fun !\"\n");
+    printf("Good day!");
+}
+void print2()
 {
     printf("David says , \" Programming is fun !\".\n");
     printf("**Conditions apply , \"Offers valid until tomorrow\"\n");
@@ -13,4 +14,22 @@ void print1()
     printf("D:/My documents/My file\n");
     printf("\\ \\ \\ \\ Today is holiday \\ \\ \\ \\\n");
     printf("This is a triple quoted string \"\"\" This month has 30 days \"\"\"\n");
+}
+
+void print() {
+    int index;
+    printf("Lutfen bir sayi girin: ");
+    scanf("%d", &index);
+    switch (index) {
+        case 0:print1();
+            break;
+        case 1:print2();
+            break;
+        default:printf("Undefined index");
+    }
+}
+
+int main() {
+    print();
+    return 0;
 }
